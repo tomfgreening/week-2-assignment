0c25ab0928a02800954b9ea56f88cd33eb9c7105
Thomas Greening <tomfgreening@gmail.com>
2024-12-13 13:21:44 +0000

    added img containers in html  and added image array in JS (IMAGES TBC)

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 console.log ("this is me testing that I have linked my JS to the HTML")
+// Leaving this in my code for now, so I can see if anything breaks down the line.
+
+const images = [
+    { src: "TBC" alt: "tbc"},
+    { src: "TBC" alt: "tbc"},
+    { src: "TBC" alt: "tbc"},
+    { src: "TBC" alt: "tbc"},
+    { src: "TBC" alt: "tbc"},
+];
